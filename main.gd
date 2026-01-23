20b8b55821cd0abac682554c2dec8796a7f81358
seukwonkang <kasworld@gmail.com>
2026-01-24 07:51:30 +0900

    use gd4template module

--- main.gd
+++ main.gd
@@ -1,32 +1,87 @@
 extends Node3D
 
-var cabinet_size := Vector3(40,22,10)
+const WorldSize := Vector3(160,90,80)
+
+func on_viewport_size_changed() -> void:
+	var vp_size := get_viewport().get_visible_rect().size
+	var 짧은길이 :float = min(vp_size.x, vp_size.y)
+	var panel_size := Vector2(vp_size.x/2 - 짧은길이/2, vp_size.y)
+	$"왼쪽패널".size = panel_size
+	$"왼쪽패널".custom_minimum_size = panel_size
+	$오른쪽패널.size = panel_size
+	$"오른쪽패널".custom_minimum_size = panel_size
+	$오른쪽패널.position = Vector2(vp_size.x/2 + 짧은길이/2, 0)
+	var msgrect := Rect2( vp_size.x * 0.1 ,vp_size.y * 0.4 , vp_size.x * 0.8 , vp_size.y * 0.25 )
+	$TimedMessage.init(vp_size.y*0.05 , msgrect, "%s %s" % [
+			ProjectSettings.get_setting("application/config/name"),
+			ProjectSettings.get_setting("application/config/version") ] )
+func timed_message_hidden(_s :String) -> void:
+	pass
+
+func label_demo() -> void:
+	if $"오른쪽패널/LabelPerformance".visible:
+		$"오른쪽패널/LabelPerformance".text = """%d FPS (%.2f mspf)
+Currently rendering: occlusion culling:%s
+%d objects
+%dK primitive indices
+%d draw calls""" % [
+		Engine.get_frames_per_second(),1000.0 / Engine.get_frames_per_second(),
+		get_tree().root.use_occlusion_culling,
+		RenderingServer.get_rendering_info(RenderingServer.RENDERING_INFO_TOTAL_OBJECTS_IN_FRAME),
+		RenderingServer.get_rendering_info(RenderingServer.RENDERING_INFO_TOTAL_PRIMITIVES_IN_FRAME) * 0.001,
+		RenderingServer.get_rendering_info(RenderingServer.RENDERING_INFO_TOTAL_DRAW_CALLS_IN_FRAME),
+		]
+	if $"오른쪽패널/LabelInfo".visible:
+		$"오른쪽패널/LabelInfo".text = "%s" % [ MovingCameraLight.GetCurrentCamera() ]
+
 
 func _ready() -> void:
-	reset_camera_pos()
-	$OmniLight3D.position = cabinet_size/2 + Vector3(0,0,cabinet_size.length())
-	$OmniLight3D.omni_range = cabinet_size.length()*2
+	on_viewport_size_changed()
+	get_viewport().size_changed.connect(on_viewport_size_changed)
+	$TimedMessage.panel_hidden.connect(timed_message_hidden)
+	$TimedMessage.show_message("",0)
+	$MovingCameraLightHober.set_center_pos_far(Vector3.ZERO, Vector3(0, 0, WorldSize.z),  WorldSize.length()*3)
+	$MovingCameraLightAround.set_center_pos_far(Vector3.ZERO, Vector3(0, 0, WorldSize.z),  WorldSize.length()*3)
+	$AxisArrow3D.set_colors().set_size(WorldSize.length()/20)
 
-	$SameGame.game_ended.connect(game_ended)
-	$SameGame.score_changed.connect(update_score_label)
-	$SameGame.init(cabinet_size)
+	$GlassCabinet.init(WorldSize)
+	same_game_demo($GlassCabinet)
 
-func game_ended(game :SameGame) -> void:
-	$SameGame.init(cabinet_size)
+var samegame :SameGame
+func same_game_demo(gc :GlassCabinet) -> void:
+	gc.show_description()
+	samegame = preload("res://same_game/same_game.tscn").instantiate(
+		).init(gc.cabinet_size)
+	gc.add_child(samegame)
+	samegame.game_ended.connect(samegame_ended)
+	samegame.score_changed.connect(update_samegame_score_label)
 
-func update_score_label(점수 :float) -> void:
-	$"왼쪽패널/점수".text = "현재점수 %d" % 점수
+func samegame_ended(_game :SameGame) -> void:
+	samegame.new_game()
 
-var camera_move = false
+func update_samegame_score_label(점수 :float) -> void:
+	samegame.get_parent().set_description_text("현재점수 %d" % 점수 )
+
+
 func _process(_delta: float) -> void:
-	var t = Time.get_unix_time_from_system() /-3.0
-	if camera_move:
-		$Camera3D.position = Vector3(sin(t)*cabinet_size.x/2, cos(t)*cabinet_size.y/2, cabinet_size.length()*0.4 ) + cabinet_size/2
-		$Camera3D.look_at(Vector3.ZERO)
+	var now := Time.get_unix_time_from_system()
+	if $MovingCameraLightHober.is_current_camera():
+		$MovingCameraLightHober.move_hober_around_z(now/2.3, Vector3.ZERO, WorldSize.length()/2, WorldSize.length()/4 )
+	elif $MovingCameraLightAround.is_current_camera():
+		$MovingCameraLightAround.move_wave_around_y(now/2.3, Vector3.ZERO, WorldSize.length()/2, WorldSize.length()/4 )
+	label_demo()
+
+func _on_button_fov_up_pressed() -> void:
+	MovingCameraLight.GetCurrentCamera().camera_fov_inc()
+
+func _on_button_fov_down_pressed() -> void:
+	MovingCameraLight.GetCurrentCamera().camera_fov_dec()
 
 var key2fn = {
 	KEY_ESCAPE:_on_button_esc_pressed,
 	KEY_ENTER:_on_카메라변경_pressed,
+	KEY_PAGEUP:_on_button_fov_up_pressed,
+	KEY_PAGEDOWN:_on_button_fov_down_pressed,
 }
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
@@ -40,11 +95,4 @@ func _on_button_esc_pressed() -> void:
 	get_tree().quit()
 
 func _on_카메라변경_pressed() -> void:
-	camera_move = !camera_move
-	if camera_move == false:
-		reset_camera_pos()
-
-func reset_camera_pos()->void:
-	$Camera3D.position = Vector3(0, 0, cabinet_size.x/2 *1.1)
-	$Camera3D.look_at(Vector3.ZERO)
-	$Camera3D.far = cabinet_size.length()
+	MovingCameraLight.NextCamera()
